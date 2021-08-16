dcbfa82e323d572f419e8fc0dde4a543a1ed25f6
rayGreycloud <raygreycloud@gmail.com>
2021-08-16 16:52:57 -0700

    expand layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,32 @@
+import Head from 'next/head';
+
 // import Header from './Header';
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className='flex flex-col items-center justify-center min-h-screen py-2 bg-gray-800 text-white'>
+      <Head>
+        <title>ray greycloud</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
       {/* <Header /> */}
-      <main>{children}</main>
-    </>
+
+      {children}
+
+      <footer className='flex items-center justify-center w-full h-24'>
+        <div className='border-t w-5/6'>
+          <a
+            className='flex items-center justify-center pt-3'
+            href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Powered by{' '}
+            <img src='/vercel.svg' alt='Vercel Logo' className='h-4 ml-2' />
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 }
