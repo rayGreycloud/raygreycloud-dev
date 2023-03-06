6092032e154bfa3fced3e2f697af3856772985bd
rayGreycloud <raygreycloud@gmail.com>
2023-03-06 14:06:33 -0800

    cursor pointer on cards

--- components/AboutNavCard.jsx
+++ components/AboutNavCard.jsx
@@ -11,7 +11,7 @@ export const AboutNavCard = () => {
       <motion.a
         variants={navCard}
         key='aboutNavCard'
-        className='p-6 border border-3 rounded-xl hover:border-green-600 focus:border-green-600 hover:text-green-600 focus:text-green-600 transition transform hover:scale-125'
+        className='cursor-pointer p-6 border border-3 rounded-xl hover:border-green-600 focus:border-green-600 hover:text-green-600 focus:text-green-600 transition transform hover:scale-125'
       >
         <div className='flex flex-col items-center justify-center w-28 h-28'>
           <ChalkboardUserIcon classes={'h-10'} />
