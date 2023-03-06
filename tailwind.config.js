cfd254cf2887b2c61545760b67d026cbcef23e1f
rayGreycloud <raygreycloud@gmail.com>
2023-03-06 14:02:31 -0800

    Update tailwindcss config w/colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      rose: colors.rose,
+      yellow: colors.yellow,
+      orange: colors.orange,
+      green: colors.green,
+      lime: colors.lime,
+      cyan: colors.cyan,
+      blue: colors.blue,
+      sky: colors.sky,
+      indigo: colors.indigo,
+      purple: colors.purple
+    },
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
